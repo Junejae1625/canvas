bc6bc6c00f52a2815ad9bf2e23a17bd36d8066e0
Jaejune <quswowns345@naver.com>
2022-08-25 18:26:36 +0900

    transition

---
+++ pages/transition/index.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const TransitionButton = styled.button`
+  width: 180px;
+  height: 40px;
+  transition: width 0.5s ease 1s, background-color 0.5s ease-in-out;
+  :hover {
+    width: 250px;
+    background-color: pink;
+  }
+`;
+
+const Transition = () => {
+  return (
+    <Wrapper>
+      <TransitionButton>마우스를 올리면 길어져요</TransitionButton>
+    </Wrapper>
+  );
+};
+export default Transition;
